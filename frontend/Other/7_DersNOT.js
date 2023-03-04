b2a3b1fbb46dab026ccd5da2ea01851f4f688bd2
suleyman <gameclub2015@mail.ru>
2023-03-04 22:07:23 +0500

    7 lesson Frontend

---
+++ frontend/Other/7_DersNOT.js
@@ -0,0 +1,6 @@
+//! ARTIK burdan itibaren FRONTEND tarafi basliyor projemizin.
+
+// 1  "npm install react-router-dom"      indirdik router ile calicaz.
+// 2   bir tane pages dosyasi actik ve icinde home.js       page olusturduk
+// 3   sonra components dosyasi olusturduk ve home.js icin birtane Navbar.js template yazdik.
+// 4   component navbar.js icinde bir tane router domun LINK tagini kullandik.
